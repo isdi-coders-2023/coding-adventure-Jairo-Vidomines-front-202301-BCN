599203ac77e60d1351e22571ddf71adc7a290430
Jairo Vidomines <jvidomines@gmail.com>
2023-02-27 17:00:03 +0100

    Add GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@fontsource/saira-stencil-one";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*, 
+*:after,
+*:before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  }
+
+#root{
+  display:flex;
+  flex-direction: column;
+
+}
+
+body {
+  padding: 20px;
+  background-color: #fff;
+  font-family: "Inter", sans-serif;
+  width: 100%;
+  min-height: 100vh;
+  
+}
+
+ol, ul, li {
+  list-style: none;
+}
+`;
+
+export default GlobalStyles;
