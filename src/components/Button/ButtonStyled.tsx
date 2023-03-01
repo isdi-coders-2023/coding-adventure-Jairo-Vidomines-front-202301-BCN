1652746184a2774bb6ff56255a61bf13cc5abe07
Jairo Vidomines <jvidomines@gmail.com>
2023-03-01 15:21:33 +0100

    Add styles to Form component

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -1,17 +1,14 @@
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
-  position: absoulte;
   width: 323.02px;
   height: 40px;
-  left: 18.99px;
-  top: 824px;
   border-radius: 10px;
   font-size: 20px;
   border: none;
   line-height: 31.48px;
   background-color: var(--main-color-button);
-  font-family: var(--main-font);
+  font-family: var(--secondary-font);
 
   :disabled {
     opacity: var(--main-color-button-disabled);
@@ -19,8 +16,6 @@ const ButtonStyled = styled.button`
 
   @media (min-width: 786px) {
     height: 57px;
-    top: 979px;
-    left: 327px;
   }
 `;
 
